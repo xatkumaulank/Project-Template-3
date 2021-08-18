4e9906a7b64cf5e92b0d368855e2d022538881f5
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-08-18 10:41:40 +0700

    Navigation Drawer + Bottom Navigation + Fragments (Fix Deprecated)

--- app/src/main/java/com/example/customnavigationdrawler/MainActivity.java
+++ app/src/main/java/com/example/customnavigationdrawler/MainActivity.java
@@ -16,6 +16,7 @@ import com.example.customnavigationdrawler.databinding.ActivityMainBinding;
 import com.example.customnavigationdrawler.fragments.FavoriteFragment;
 import com.example.customnavigationdrawler.fragments.HistoryFragment;
 import com.example.customnavigationdrawler.fragments.HomeFragment;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -35,27 +36,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
         viewPagerAdapter = new ViewPagerAdapter(this);
-        binding.viewPager2.setAdapter(viewPagerAdapter);
+        //binding.viewPager2.setAdapter(viewPagerAdapter);
 
 
-        new TabLayoutMediator(binding.tabLayout, binding.viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                switch (position){
-                    case 0:
-                        tab.setText(getString(R.string.nav_home));
-                        break;
-
-                    case 1:
-                        tab.setText(getString(R.string.nav_favorite));
-                        break;
-
-                    case 2:
-                        tab.setText(getString(R.string.nav_history));
-                        break;
-                }
-            }
-        }).attach();
+//        new TabLayoutMediator(binding.tabLayout, binding.viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+//            @Override
+//            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+//                switch (position){
+//                    case 0:
+//                        tab.setText(getString(R.string.nav_home));
+//                        break;
+//
+//                    case 1:
+//                        tab.setText(getString(R.string.nav_favorite));
+//                        break;
+//
+//                    case 2:
+//                        tab.setText(getString(R.string.nav_history));
+//                        break;
+//                }
+//            }
+//        }).attach();
 
         setSupportActionBar(binding.toolBar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,binding.drawerLayout,binding.toolBar,
@@ -65,59 +66,90 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         binding.navigationView.setNavigationItemSelectedListener(this);
 
-        //replaceFragment(new HomeFragment());
+        replaceFragment(new HomeFragment());
         binding.navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+        binding.bottomNavigation.getMenu().findItem(R.id.bottom_home).setChecked(true);
 
-        binding.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+
+        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                switch (position){
-                    case 0:
-                        currentFragment = FRAGMENT_HOME;
-                        binding.navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
-                        break;
-
-                    case 1:
-                        currentFragment = FRAGMENT_FAVORITE;
-                        binding.navigationView.getMenu().findItem(R.id.nav_favorite).setChecked(true);
-                        break;
-
-                    case 2:
-                        currentFragment = FRAGMENT_HISTORY;
-                        binding.navigationView.getMenu().findItem(R.id.nav_history).setChecked(true);
-                        break;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.bottom_home){
+                    replaceHomeFragment();
+                    binding.navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+                }else  if (id == R.id.bottom_favorite){
+                    replaceFavoriteFragment();
+                    binding.navigationView.getMenu().findItem(R.id.nav_favorite).setChecked(true);
+                }else  if (id == R.id.bottom_history){
+                    replaceHistoryFragment();
+                    binding.navigationView.getMenu().findItem(R.id.nav_history).setChecked(true);
                 }
+                return true;
             }
         });
+//        binding.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+//            @Override
+//            public void onPageSelected(int position) {
+//                super.onPageSelected(position);
+//                switch (position){
+//                    case 0:
+//                        currentFragment = FRAGMENT_HOME;
+//                        binding.navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+//                        break;
+//
+//                    case 1:
+//                        currentFragment = FRAGMENT_FAVORITE;
+//                        binding.navigationView.getMenu().findItem(R.id.nav_favorite).setChecked(true);
+//                        break;
+//
+//                    case 2:
+//                        currentFragment = FRAGMENT_HISTORY;
+//                        binding.navigationView.getMenu().findItem(R.id.nav_history).setChecked(true);
+//                        break;
+//                }
+//            }
+//        });
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.nav_home){
-            if (currentFragment != FRAGMENT_HOME){
-                //replaceFragment(new HomeFragment());
-                binding.viewPager2.setCurrentItem(0);
-                currentFragment = FRAGMENT_HOME;
-            }
+            replaceHomeFragment();
+            binding.bottomNavigation.getMenu().findItem(R.id.bottom_home).setChecked(true);
         }else if (id == R.id.nav_favorite){
-            if (currentFragment != FRAGMENT_FAVORITE){
-                //replaceFragment(new FavoriteFragment());
-                binding.viewPager2.setCurrentItem(1);
-                currentFragment = FRAGMENT_FAVORITE;
-            }
+            replaceFavoriteFragment();
+            binding.bottomNavigation.getMenu().findItem(R.id.bottom_favorite).setChecked(true);
         }else if (id == R.id.nav_history){
-            if (currentFragment != FRAGMENT_HISTORY){
-                //replaceFragment(new HistoryFragment());
-                binding.viewPager2.setCurrentItem(2);
-                currentFragment = FRAGMENT_HISTORY;
-            }
+           replaceHistoryFragment();
+            binding.bottomNavigation.getMenu().findItem(R.id.bottom_history).setChecked(true);
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-
+    private void replaceHomeFragment(){
+        if (currentFragment != FRAGMENT_HOME){
+            replaceFragment(new HomeFragment());
+            //binding.viewPager2.setCurrentItem(0);
+            currentFragment = FRAGMENT_HOME;
+        }
+    }
+    private void replaceFavoriteFragment(){
+        if (currentFragment != FRAGMENT_FAVORITE){
+            replaceFragment(new FavoriteFragment());
+            //binding.viewPager2.setCurrentItem(1);
+            currentFragment = FRAGMENT_FAVORITE;
+        }
+    }
+    private void replaceHistoryFragment(){
+        if (currentFragment != FRAGMENT_HISTORY){
+            replaceFragment(new HistoryFragment());
+            //binding.viewPager2.setCurrentItem(2);
+            currentFragment = FRAGMENT_HISTORY;
+        }
+    }
     @Override
     public void onBackPressed() {
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
@@ -126,9 +158,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-//    private void replaceFragment(Fragment fragment){
-//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        fragmentTransaction.replace(R.id.content_frame,fragment);
-//        fragmentTransaction.commit();
-//    }
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame,fragment);
+        fragmentTransaction.commit();
+    }
 }
