d7ceb5219ceb7d54face141fcf6a49faee1e0a21
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-08-18 09:45:29 +0700

    Navigation Drawer + TabLayout + ViewPager2 + Fragments (Fix Deprecated)

--- app/src/main/java/com/example/customnavigationdrawler/MainActivity.java
+++ app/src/main/java/com/example/customnavigationdrawler/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -16,6 +17,8 @@ import com.example.customnavigationdrawler.fragments.FavoriteFragment;
 import com.example.customnavigationdrawler.fragments.HistoryFragment;
 import com.example.customnavigationdrawler.fragments.HomeFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final int FRAGMENT_HOME = 0;
@@ -23,12 +26,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public static final int FRAGMENT_HISTORY = 2;
 
     private int currentFragment = FRAGMENT_HOME;
+    private ViewPagerAdapter viewPagerAdapter;
 
     ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        binding.viewPager2.setAdapter(viewPagerAdapter);
+
+
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText(getString(R.string.nav_home));
+                        break;
+
+                    case 1:
+                        tab.setText(getString(R.string.nav_favorite));
+                        break;
+
+                    case 2:
+                        tab.setText(getString(R.string.nav_history));
+                        break;
+                }
+            }
+        }).attach();
 
         setSupportActionBar(binding.toolBar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,binding.drawerLayout,binding.toolBar,
@@ -38,8 +65,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         binding.navigationView.setNavigationItemSelectedListener(this);
 
-        replaceFragment(new HomeFragment());
+        //replaceFragment(new HomeFragment());
         binding.navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+
+        binding.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position){
+                    case 0:
+                        currentFragment = FRAGMENT_HOME;
+                        binding.navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+                        break;
+
+                    case 1:
+                        currentFragment = FRAGMENT_FAVORITE;
+                        binding.navigationView.getMenu().findItem(R.id.nav_favorite).setChecked(true);
+                        break;
+
+                    case 2:
+                        currentFragment = FRAGMENT_HISTORY;
+                        binding.navigationView.getMenu().findItem(R.id.nav_history).setChecked(true);
+                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -47,17 +97,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
         if (id == R.id.nav_home){
             if (currentFragment != FRAGMENT_HOME){
-                replaceFragment(new HomeFragment());
+                //replaceFragment(new HomeFragment());
+                binding.viewPager2.setCurrentItem(0);
                 currentFragment = FRAGMENT_HOME;
             }
         }else if (id == R.id.nav_favorite){
             if (currentFragment != FRAGMENT_FAVORITE){
-                replaceFragment(new FavoriteFragment());
+                //replaceFragment(new FavoriteFragment());
+                binding.viewPager2.setCurrentItem(1);
                 currentFragment = FRAGMENT_FAVORITE;
             }
         }else if (id == R.id.nav_history){
             if (currentFragment != FRAGMENT_HISTORY){
-                replaceFragment(new HistoryFragment());
+                //replaceFragment(new HistoryFragment());
+                binding.viewPager2.setCurrentItem(2);
                 currentFragment = FRAGMENT_HISTORY;
             }
         }
@@ -73,9 +126,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-    private void replaceFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.content_frame,fragment);
-        fragmentTransaction.commit();
-    }
+//    private void replaceFragment(Fragment fragment){
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.content_frame,fragment);
+//        fragmentTransaction.commit();
+//    }
 }
